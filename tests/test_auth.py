46abd44fcf4b62faa3a3c20e473eb3dc82b1dd33
MooseyAnon <moooseyanon@gmail.com>
2023-08-17 15:14:33 +0100

    corna->test_utils: Add new test file to test utils
    This commit adds a new test file to test functionality in the utilities
    directory.
    
    It also moves any tests that are better to that file.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,7 +2,7 @@ import pytest
 
 from corna import enums
 from corna.db import models
-from corna.utils import encodings, secure
+from corna.utils import secure
 from tests.shared_data import single_user
 
 
@@ -303,34 +303,3 @@ def test_token_is_valid(session, client, login):
     unsigned_cookie = secure.decoded_message(cookie)
     db_cookie = session.query(models.SessionTable).all()[0].cookie_id
     assert db_cookie == unsigned_cookie
-
-
-def test_signing_similar_messages():
-
-    m1 = "aaaaaaaaa"
-    m2 = "aaaaaaaab"
-
-    s1 = secure.sign(m1)
-    s2 = secure.sign(m2)
-
-    _, s1_sig = secure.unsign(s1)
-    _, s2_sig = secure.unsign(s2)
-
-    assert secure.verify(m1, s1_sig)
-    assert secure.verify(m2, s2_sig)
-
-    assert not secure.verify(m1, s2_sig)
-    assert not secure.verify(m2, s1_sig)
-
-
-def test_unsign():
-
-    sig = secure.sign("aaaaaaaaa")
-    orig_message, _ = secure.unsign(sig)
-    assert orig_message == b"aaaaaaaaa"
-
-
-def test_fake_message():
-    message = "I am a fake message"
-    assert secure.verify(message, encodings.base64_encode(message)) == False
-
