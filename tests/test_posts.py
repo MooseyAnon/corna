64b1cf87f5ec2ccda7bc701842cb99ca6a7def6c
MooseyAnon <moooseyanon@gmail.com>
2023-08-17 01:55:14 +0100

    corna->post_control.py: Use hash to name directory
    Previously, we were using the current date as a directory naming scheme in order
    to create unique directories for each incoming image/imageset.
    
    However, while we want a decently balanced file system without clusters of
    images we also do not need one directory per image. Using the image hash to
    build the image fs allows use to create a balanced fs which uses any shared
    prefixes while also eventually putting any unique image in its own directory.
    
    Using hash's also gives us the benefit that if there are any duplicates we will
    know as the directory will already exist and we can either save the new file
    or just use the original. However, none of these latter behaviours are
    implemented and are just thoughts for the future.

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -34,6 +34,10 @@ def _all_post_based_stubs(tmpdir, mocker, monkeypatch):
         "corna.controls.post_control.random_short_string",
         return_value="abcdef",
     )
+    mocker.patch(
+        "corna.utils.image_proc.hash_image",
+        return_value="thisisafakehash12345",
+    )
     monkeypatch.setattr(
         post_control,
         "PICTURE_DIR",
@@ -96,7 +100,7 @@ def test_post_with_picture(session, client, blog):
         data=shared_data.post_with_picture
     )
 
-    expected_path = assets / utils.get_utc_now().date().isoformat()
+    expected_path = assets / "thi/sis/afa/kehash12345"
     assert expected_path.exists()
     assert len(expected_path.listdir()) == 1
     for file in expected_path.listdir():
@@ -214,7 +218,7 @@ def test_get_image(session, client, blog):
     )
     assert resp.status_code == 201
 
-    expected_path = assets / utils.get_utc_now().date().isoformat()
+    expected_path = assets / "thi/sis/afa/kehash12345"
     assert expected_path.exists()
 
     full_path = expected_path.listdir()[0]
@@ -224,4 +228,41 @@ def test_get_image(session, client, blog):
         "abcdef") == full_path
 
 
+def test_path_collision(session, client, capsys, blog):
+    assets = post_control.PICTURE_DIR
 
+    # opening the picture file in a shared directory fucks things up
+    # over multiple runs, so its easier just to put it in here
+    post_with_picture = {
+        "type": "picture",
+        "caption": "this is a picture I did not take",
+        "pictures": (shared_data.ASSET_DIR / "anders-jilden.jpg").open("rb"),
+    }
+
+    resp = client.post(
+        f"/api/v1/posts/{shared_data.blog_info['domain_name']}",
+        data=post_with_picture
+    )
+    assert resp.status_code == 201
+    # check pic exists
+    assert len((assets / "thi/sis/afa/kehash12345").listdir()) == 1
+
+    # write picture to tmpdir to use again
+    import shutil
+    shutil.copy(
+        (shared_data.ASSET_DIR / "anders-jilden.jpg"),
+        (assets / "same-pic-different-name.jpg")
+    )
+
+    assert (assets / "same-pic-different-name.jpg").exists()
+
+    # putting it into the same file should raise a FileExistsError
+    # but should still be saved
+    from werkzeug.datastructures import FileStorage
+    file = FileStorage(filename=str(assets / "same-pic-different-name.jpg"))
+    post_control.handle_pictures(file)
+
+    captured = capsys.readouterr()
+    assert "Photo directory exists, duplicate?" in captured.err
+
+    assert len((assets / "thi/sis/afa/kehash12345").listdir()) == 2
