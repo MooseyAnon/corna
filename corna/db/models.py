f98035b374aba56e57348ddb7a428178ed338929
MooseyAnon <moooseyanon@gmail.com>
2023-09-13 18:50:56 +0100

    corna->models.py: add themes table
    This commit add a themes table for basic themes management.

--- corna/db/models.py
+++ corna/db/models.py
@@ -332,3 +332,42 @@ class PhotoPost(Base):
         uselist=False,
         back_populates="photo",
     )
+
+
+class Themes(Base):
+    """Table for managing corna themes."""
+
+    uuid = Column(
+        UUID,
+        primary_key=True,
+    )
+    name = Column(
+        Text,
+        doc="The name of the theme",
+    )
+    description = Column(
+        Text,
+        doc="Short, optional, description of the theme",
+    )
+    created = Column(
+        DateTime,
+        doc="Date theme was created",
+    )
+    path = Column(
+        Text,
+        doc="Path to the main index.html of the theme, relative to themes "
+            "directory.",
+    )
+    status = Column(
+        Text,
+        required=True,
+        doc="The current status of the PR i.e. do we know the full "
+            "path to the theme yet? This is needed because there may "
+            "be a delay between the initial time that the theme gets "
+            "submitted and the time we know the full path to the theme",
+    )
+    creator_user_id = Column(
+        UUID,
+        ForeignKey("users.uuid"),
+        doc="User who created the theme, for attribution",
+    )
