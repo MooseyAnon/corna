e66012c5006e2cd9ab20e2092500e678df6b8cc0
MooseyAnon <moooseyanon@gmail.com>
2023-12-12 16:12:05 +0000

    frontend->lib: Add frontend utilities
    This commit adds utility files that are used across the frontend source code

---
+++ frontend/ts-src/lib/network.ts
@@ -0,0 +1,124 @@
+/* Corna frontend network utilities. */
+
+// axios types: https://github.com/axios/axios/blob/v1.x/index.d.ts
+import axios from "axios";
+import {
+    AxiosError,
+    AxiosHeaders,
+    AxiosPromise,
+    AxiosRequestConfig,
+    AxiosResponse,
+} from "axios";
+
+import { handlePromise } from "./utils.js";
+
+
+export function getApiUrl(): string {
+    /* Get the correct API URL.
+    *
+    * This is here to make local development easier.
+    * @returns { string }
+    */
+    const currUrl: string = window.location.hostname;
+    const apiUrl: string = (
+        (currUrl === "localhost" || currUrl === "127.0.0.1")
+        ? "http://127.0.0.1:8080/api"
+        : "https://api.mycorna.com"
+    )
+    return apiUrl;
+}
+
+
+export function request<T>(
+    urlExtension: string,
+    method: ("get" | "delete" | "post" | "put") = "get",
+    payload?: T,
+    headers?: AxiosHeaders,
+): AxiosPromise {
+    /* Generalised requestion function that wraps axios.request.
+    *
+    * @param { string } urlExtension: the API url extension
+    * @param { ("get" | "delete" | "post" | "put") } method:
+    *     http method for the request
+    * @param { optional<T> } payload: any payload to send
+    * @param { optional<AxiosHeaders> } headers: any headers to send
+    *     with request
+    * @returns { AxiosPromise }
+    */
+
+    const url: string = `${getApiUrl()}/${urlExtension}`;
+    const requestConf: AxiosRequestConfig = {
+        method: method,
+        url: url,
+        data: payload,
+        withCredentials: true,
+        headers: headers,
+
+    }; 
+    return axios.request(requestConf);
+}
+
+
+export function handleNetworkError(
+    error: AxiosError,
+    errorMessage: string = "There was a problem, please try again :(",
+): string {
+    /* Handle any network errors.
+    *
+    * @param { AxiosError } error: network error
+    * @param { string } errorMessage: a generic error message to be used
+    *     in case error does not come with message
+    * @return { string }
+    */
+    let errMsg: string;
+
+    if (error.status && error.status === 422) {
+        errMsg = "There is an error on our end, sorry! Please try again."
+    }
+
+    else if (error.response) {
+        const er: AxiosResponse = error.response;
+        errMsg = er.data.message ? er.data.message : errorMessage;
+    }
+
+    else {
+        errMsg = errorMessage;
+    }
+
+    return errMsg;
+}
+
+
+export async function postData<T, E>(
+    data: T,
+    urlExtension: string,
+    errorMessageCallback: (errMsg: string) => E,
+    redirectUrlExtension?: string,
+    redirectCallback?: (urlExtension: string) => Promise<void>,
+): Promise<void> {
+    /* Post user data to backend.
+    *
+    * @param { T } data: user data to send
+    * @param { string } urlExtension: the API url extension to call
+    * @param { (errMsg: string) => any }: callback for error handling
+    * @param { string } redirectUrlExtension: optional redirect url
+    * @param { (urlExtension: string) => Promise<void> } redirectCallback:
+    *     optional redirect callback (the backend will do the actual
+    *     redirect)
+    * @returns { Promise<void> }
+    */
+    await (async () => {
+        const [error, response] = await handlePromise(
+            request(urlExtension, "post", data))
+
+        if (response && (redirectUrlExtension && redirectCallback)) {
+            redirectCallback(redirectUrlExtension)
+        }
+
+        else if (error) {
+            const errMsg: string = handleNetworkError(error as AxiosError);
+            errorMessageCallback(errMsg);
+        }
+    })();
+
+}
---
+++ frontend/ts-src/lib/utils.ts
@@ -0,0 +1,176 @@
+/* Utility functions. */
+
+
+export function isEmail(email: string): boolean {
+    /* Very, very basic greedy email regex.
+    *
+    * stolen from: https://stackoverflow.com/a/4964766
+    * Essentially here we are testing for the following:
+    *    - '@' symbol has at least 1 char before it
+    *    - '@' symbol has at least 1 char after it
+    *    - '.' (period) has at least one char after it (although might omit this
+    *      or at least make it optional)
+    *
+    * @param { string } email: the email being checked
+    * @returns { boolean }
+    */
+
+    const regex: RegExp = /^\S+@\S+\.\S+$/;
+    return regex.test(email);
+}
+
+
+export function spaceAtStart(str: string): boolean {
+    /* Check for space at the start of a string. 
+    *
+    * Spaces are allowed inside passwords but not at the start or
+    * end because this can lead to subtle issues where users
+    * accidentally add a space before/after the password without
+    * realising.
+    *
+    * @param { string } str: the string being checked
+    * @returns { boolean }
+    */
+
+    const regex: RegExp = /^\s+/;
+    return regex.test(str)
+}
+
+
+export function spaceAtEnd(str: string): boolean {
+    /* Check for space at the end of a string. 
+    *
+    * Spaces are allowed inside passwords but not at the start or
+    * end because this can lead to subtle issues where users
+    * accidentally add a space before/after the password without
+    * realising.
+    *
+    * @param { string } str: the string being checked
+    * @returns { boolean }
+    */
+
+    const regex: RegExp = /\s+$/;
+    return regex.test(str)
+}
+
+
+export function hasSpace(str: string): boolean {
+    /* Check if a string contains any space characters.
+    *
+    * @param { string } str: the string being checked
+    * @returns { boolean }
+    */
+
+    const regex: RegExp = /\s/;
+    return regex.test(str);
+}
+
+
+export function hasCapitalLetter(str: string): boolean {
+    /* Ensure a string has at least one capital letter.
+    *
+    * @param { string } str: the string to check against
+    * @return { boolean }
+    */
+
+    const regex: RegExp = /[A-Z]/;
+    return regex.test(str);
+}
+
+
+export function hasDigit(str: string): boolean {
+    /* Ensure a string has at least one numerical digit.
+    *
+    * @param { string } str: the string to check against
+    * @return { boolean }
+    */
+
+    const regex: RegExp = /\d/;
+    return regex.test(str);
+}
+
+
+export function clean(str: string): string {
+    /* Make string lower case and remove spaces at start and end.
+    *
+    * @param { string } str: the string being cleaned
+    * @returns { string }
+    */
+
+    return str.toLowerCase().trim();
+}
+
+
+// html element list:
+// https://microsoft.github.io/PowerBI-JavaScript/interfaces/_node_modules_typedoc_node_modules_typescript_lib_lib_dom_d_.htmlelement.html
+export function createElement(
+    type: string,
+    classList: string[] = []
+): HTMLElement {
+    /* HTML create factory. 
+    *
+    * Callers need to cast to the appropriate element type.
+    *
+    * @param { string } type: the type of html element to make
+    *     e.g. 'div', 'img', 'input' etc
+    * @param { string[] } classList: optional CSS classes to add
+    *     to the new element
+    * @returns { HTMLElement }
+    */
+
+    const newElement = document.createElement(type) as HTMLElement;
+    if (classList) { newElement.classList.add(...classList); }
+    return newElement;
+}
+
+
+export function createDivElement(
+    classList: string[] = [],
+    textContent?: string
+): HTMLDivElement {
+    /* Create a new div element.
+    *
+    * @param { string[] } classList: CSS classes to add to new element
+    * @param { string } textContent: Optional textContent to add to
+    *     the new element
+    * @returns { HTMLDivElement }
+    */
+
+    const newDiv = createElement("div", classList) as HTMLDivElement;
+    if (textContent) { newDiv.textContent = textContent; }
+    return newDiv;
+}
+
+
+export function createImageElement(
+    classList: string[] = [],
+    src?: string
+): HTMLImageElement {
+    /* Create a new img element.
+    *
+    * @param { string[] } classList: CSS classes to add to new element
+    * @param { string } src: image source href
+    * @returns { HTMLImageElement }
+    */
+
+    const newImage = createElement("img", classList) as HTMLImageElement;
+    if (src) { newImage.src = src; }
+    return newImage;
+}
+
+
+// This is copied from here:
+// - https://github.com/tysoncadenhead/cartesian-js/blob/master/src/handle.ts
+export function handlePromise<E, T>(
+    promise: Promise<unknown>
+): Promise<[E, T]> {
+    /* Generalised promise handling.
+    *
+    * @param { Promise<unknown> } promise
+    * @returns { Promise<[E, T]> }
+    */
+
+    return promise
+    .then((data) => Promise.resolve([undefined, data]) as Promise<[E, T]>)
+    .catch((error) => Promise.resolve([error, undefined]) as Promise<[E, T]>);
+}
